2079739c3012e4ee8dafce545d857f53c52cc6ab
hwain <immortal0900@gmail.com>
2025-12-20 01:32:30 +0900

    refactor: sage_mapping

--- src/db/user_memory_models.py
+++ src/db/user_memory_models.py
@@ -149,6 +149,7 @@ NPC_ID_TO_HEROINE = {0: "sage", 1: "letia", 2: "lupames", 3: "roco"}
 
 # heroine_id -> Speaker Enum 변환
 HEROINE_TO_SPEAKER = {
+    "sage": Speaker.SAGE,
     "letia": Speaker.LETIA,
     "lupames": Speaker.LUPAMES,
     "roco": Speaker.ROCO,
